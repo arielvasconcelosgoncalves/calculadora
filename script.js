58b99e519ac987fa06714024eec1653c198fbd33
Ariel Vasconcelos <ariel.goncalves@sou.unifeob.edu.br>
2025-05-13 16:57:06 -0300

    feat: duas function de verificação

--- script.js
+++ script.js
@@ -2,12 +2,37 @@ function insert(num){
             var numero = document.getElementById('result').innerHTML;
             document.getElementById('result').innerHTML = numero + num;
     }
+function insertOperator(num){
+        var resultado = document.getElementById('result').innerHTML;
+        if(resultado.charAt(resultado.length - 1) === '+' 
+        || resultado.charAt(resultado.length - 1) === '-' 
+        || resultado.charAt(resultado.length - 1) === '/' 
+        || resultado.charAt(resultado.length - 1) === '*'){
+
+            
+        }
+        else{
+            var numero = document.getElementById('result').innerHTML;
+            document.getElementById('result').innerHTML = numero + num;
+        }
+            
+    }
+function insertPoint(num){
+            var resultado = document.getElementById('result').innerHTML;
+        if(resultado.charAt(resultado.length - 1) === '.'){
+          
+        }
+        else{
+            var numero = document.getElementById('result').innerHTML;
+            document.getElementById('result').innerHTML = numero + num;
+        }
+    }
 function clean(){
             document.getElementById('result').innerHTML = "";
     }
 function back(){
             var resultado = document.getElementById('result').innerHTML;
-            document.getElementById('reslt').innerHTML = resultado.substring(0, resultado.length -1);
+            document.getElementById('result').innerHTML = resultado.substring(0, resultado.length -1);
     }
 function execute(){
         var resultado = document.getElementById('result').innerHTML;
